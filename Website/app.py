e1c61b6db392cd82d43125a292268b0218b7e06a
binglinten01 <binglinten01@mail.ru>
2025-02-13 21:30:20 +0700

    Creates a website template

---
+++ Website/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Phone Sensor", page_icon="./sensor.png", layout="wide")
+
+
+
+# ----Header Section----
+st.title("%Project Name%")
+st.subheader("This is a subheader.")
+st.write("This is a write.")
+st.write("[This is a hyperlink. >](https://github.com/binglinten01/Smart-Sleep-Tracker)")
